1a7f51791cf46aff6e7c8254ffdb97db8fe2d0e8
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2019-11-04 17:11:44 -0500

    Se agrego el método GET

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+''' 
+    jsonify. Nos permite convertir
+    un objeto a un json
+'''
+
 '''Aplicativo de servidorr'''
 app = Flask(__name__)
 
@@ -8,10 +14,41 @@ app = Flask(__name__)
 
 from products import products
 
-''' Testeo. Ruta de prueba '''
+''' Testeo. Ruta de prueba - string '''
 @app.route('/ping')
 def ping():
     return 'Pong!'
 
+''' Testeo: JSON '''
+@app.route('/demo_json')
+def demo():
+    return jsonify({"messsage": "pong!!"})
+
+''' Por defecto se trabaja con method=['GET'] '''
+@app.route('/product')
+def getProduct():
+    return jsonify(products)
+
+@app.route('/products')
+def getProducts():
+    return jsonify({"products": products})
+
+@app.route('/productsList')
+def getProductsList():
+    return jsonify({"products": products, "message": "Product's List"})
+
+@app.route('/productsList/<string:product_name>')
+def getProductName(product_name):
+    print(product_name)
+    productsFound = [product for product in products if product['name'] == product_name]
+    if (len(productsFound) > 0):
+        return jsonify({"Product": productsFound[0]})
+    return jsonify({"message": "Product not found"})
+
+'''
+@app.route('/product', methods=['POST'])
+def addProduct():
+'''
+    
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
